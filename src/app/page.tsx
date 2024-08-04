5ad91d931e000b37a5efb5a27c358d6e2f0566a2
lucassodresa <lucas.sodresa@gmail.com>
2024-08-04 09:00:31 +0100

    reset the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
-
 export default async function Home() {
-  const session = await getServerSession();
-
-  if (!session?.user) return redirect("/api/auth/signin");
-
-  console.log(session);
-
   return (
     <main>
-      <h1>Welcome, {session.user.name}</h1>
+      <h1>Welcome, </h1>
     </main>
   );
 }
