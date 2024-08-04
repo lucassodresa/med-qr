397bda26e9a59076b7ce0eb7a879f5dfe79182b9
lucassodresa <lucas.sodresa@gmail.com>
2024-08-04 17:11:16 +0100

    login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,10 @@
-import Link from "next/link";
+import Image from "next/image";
 import { headers } from "next/headers";
-import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
 import { SubmitButton } from "./submit-button";
+import loginCoverPhoto from "@/public/images/login-cover-photo.webp";
+
 const signInWithGoogle = async () => {
   "use server";
 
@@ -21,12 +23,28 @@ const signInWithGoogle = async () => {
 
 export default function Login() {
   return (
-    <div>
-      <form>
-        <SubmitButton formAction={signInWithGoogle} pendingText="Signing In...">
-          Login with Google
-        </SubmitButton>
-      </form>
+    <div className="w-full h-screen lg:grid lg:grid-cols-2">
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Login</h1>
+            <p className="text-balance text-muted-foreground">
+              Enter your email below to login to your account
+            </p>
+          </div>
+          <form className="grid gap-4">
+            <SubmitButton
+              formAction={signInWithGoogle}
+              pendingText="Signing In..."
+            >
+              Login with Google
+            </SubmitButton>
+          </form>
+        </div>
+      </div>
+      <div className="hidden  lg:block overflow-hidden">
+        <Image src={loginCoverPhoto} alt="Image" sizes="50vw" priority={true} />
+      </div>
     </div>
   );
 }
--- app/login/submit-button.tsx
+++ app/login/submit-button.tsx
@@ -1,20 +1,19 @@
 "use client";
 
 import { useFormStatus } from "react-dom";
-import { type ComponentProps } from "react";
+import { Button, ButtonProps } from "@/components/ui/button";
 
-type Props = ComponentProps<"button"> & {
+type Props = ButtonProps & {
   pendingText?: string;
 };
 
 export function SubmitButton({ children, pendingText, ...props }: Props) {
   const { pending, action } = useFormStatus();
-
   const isPending = pending && action === props.formAction;
 
   return (
-    <button {...props} type="submit" aria-disabled={pending}>
+    <Button {...props} type="submit" aria-disabled={pending}>
       {isPending ? pendingText : children}
-    </button>
+    </Button>
   );
 }
